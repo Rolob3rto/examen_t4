90ca0f9609aa78fb693ab963732437e122182a96
Rolob3rto <robertooli98@gmail.com>
2023-01-30 11:13:06 +0100

    commit incial

---
+++ src/main/java/com/roloberto/examen_t4/ExamenT4Application.java
@@ -0,0 +1,13 @@
+package com.roloberto.examen_t4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenT4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenT4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/roloberto/examen_t4/ExamenT4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roloberto.examen_t4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenT4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
